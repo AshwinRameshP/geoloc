890d8b74e85df112e674e45e8e737b66b150dad9
AshwinRameshP <ashwinrameshp@gmail.com>
2019-01-31 20:30:09 +0530

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,22 @@
-document.getElementById("bt1").addEventListener("click",getPosition);
-function getPosition(){
+document.getElementById("bt1").addEventListener("click", getPosition);
+document.getElementById("bt2").addEventListener("click", getMap);
+function getPosition() {
+    var watchID = navigator.geolocation.getCurrentPosition(onSuccess,onError);
+    
+    function onSuccess(position) {
+alert("Latitude:" + position.coords.latitude+"\n"+"Longitude:"+position.coords.longitude+"\n");
+            }
+    function onError(error){
+        alert('code: '+error.code+ "\n"+ "message:"+error.message+"\n");
+    }
+    
+}
+function getMap() {
     var watchID=navigator.geolocation.getCurrentPosition(onSuccess,onError);
     
     function onSuccess(position){
-alert("Latitude:" + position.coords.latitude+"\n"+"Longitude:"+position.coords.longitude+"\n");
-        
-window.open("https://www.latlong.net/c/?lat="+position.coords.latitude+"&long="+position.coords.longitude)
+
+window.location="https://www.latlong.net/c/?lat="+position.coords.latitude+"&long="+position.coords.longitude;  
     }
     function onError(error){
         alert('code: '+error.code+ "\n"+ "message:"+error.message+"\n");
